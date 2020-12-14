9b3c06602310e092d26f6681e5d474bdd0e63091
Auke van Slooten <auke@muze.nl>
2020-12-14 15:48:16 +0100

    added review and committing changes

--- js/importer.js
+++ js/importer.js
@@ -40,6 +40,7 @@
 					}
 					if (!errors.length && !validations.length) {
 						console.log('contexts',contexts);
+						tree.showChanges(document.querySelector('.slo-tree-changes'), contexts);
 					} else {
 						console.log(errors);
 						console.log(validations);
@@ -87,6 +88,20 @@
                     }
                 });
             },
+			'handle-changes': function(form, values) {
+				var schemas = {};
+				if (!Array.isArray(values.applyChanges)) {
+					if ( !values.applyChanges ) {
+						alert('Selecteer tenminste 1 context');
+						return;
+					}
+					values.applyChanges = [ values.applyChanges ];
+				}
+				values.applyChanges.forEach(schema => {
+					schemas[schema] = editor.pageData.changedSchemas[schema];
+				});
+				return importTool.actions['handle-changes'](schemas);
+			},
             'delete-change' : function(el) {
                 // find the id of the entity;
                 var id = el.closest('.slo-entiteit-change').querySelector("a").innerHTML;
@@ -198,6 +213,75 @@
                     }
                 });
             },
+			'handle-changes': function(schemas) {
+				var changes = [];
+				var changedIds = {};
+				var changesType = {};
+				var changesSchema = {};
+				Object.keys(schemas).forEach(schema => {
+					Object.keys(schemas[schema]).forEach(type => {
+						console.log('building change set: '+type+' ('+schemas[schema][type].length+')');
+						schemas[schema][type].forEach(entity => {
+							var original = curriculum.index.id[entity.id];
+							if (!original) {
+								original = {};
+								Object.assign(original, entity); // overwrite information from entity into the original entity
+								curriculum.schema[schema][type].push(original);
+								curriculum.data[type].push(original);
+							} else {
+								Object.assign(original, entity); // overwrite information from entity into the original entity
+								var index = curriculum.schema[schema][type].findIndex(e => e.id === entity.id);
+								if (index<0) {
+									throw new Error('Kan originele entiteit niet vinden');
+								}
+								curriculum.schema[schema][type].splice(index, 1, original);
+
+								var index = curriculum.data[type].findIndex(e => e.id === entity.id);
+								if (index<0) {
+									throw new Error('Kan originele entiteit niet vinden');
+								}
+								curriculum.data[type].splice(index, 1, original);
+							}
+							changes.push(original);
+							curriculum.index.id[entity.id] = original;
+							curriculum.index.type[entity.id] = type;
+							curriculum.index.schema[entity.id] = schema;
+						});
+					});
+				});
+
+				// check that no entity in the selected schemas has a reference
+				// to an item that isn't in the selected list or in the current
+				// curriculum dataset
+				var hasMissingReferences = function(e) {
+					var refProps = Object.keys(e).filter(p => Array.isArray(e[p]));
+					for (var i=0,l=refProps.length; i<l; i++) {
+						for (var ii=0,ll=e[refProps[i]].length; ii<ll; ii++) {
+							var id = e[refProps[i]][ii];
+							if (!changedIds[id] && !curriculum.index.id[id]) {
+								return true;
+							}
+						}
+					}
+					return false;
+				};
+
+				var missingRefs = false;
+				for (var i=0,l=changes.length; i<l; i++) {
+					if (hasMissingReferences(changes[i])) {
+						missingRefs = true;
+						break;
+					}
+				}
+				if (missingRefs) {
+					alert('Deze selectie bevat entiteiten met referenties naar nog niet bestaande entiteiten die nu niet mee geimporteerd worden. Pas de selectie aan a.u.b.');
+				} else {
+					Object.keys(changesType).forEach(id => curriculum.index.type[id] = changesType[id]);
+					Object.keys(changesSchema).forEach(id => curriculum.index.schema[id] = changesSchema[id]);
+					editor.pageData.changes = changes;
+					editor.pageData.changeCount = changes.length;
+				}
+			},
             'commit-changes': function(changes, message) {
                 if (!message) {
                     return Promise.reject('Commit message required');
@@ -208,25 +292,13 @@
                     if (!change.commit) {
                         return false;
                     }
-                    var id = change.id;
-                    var original = curriculum.data[curriculum.index.type[id]].find(function(entry) {
-                        if (entry.id == id) {
-                            return true;
-                        }
-                        return false;
-                    });
-                    if (original) {
-                        var index = curriculum.data[curriculum.index.type[id]].indexOf(original);
-                        curriculum.data[curriculum.index.type[id]][index] = change;
-                    } else {
-                        curriculum.data[curriculum.index.type[id]].push(change);
-                    }
                     var section = curriculum.index.type[change.id];
                     var schema = curriculum.getSchemaFromType(section);
                     if (!schemas[schema]) {
-                        schemas[schema] = [];
+                        schemas[schema] = {};
                     }
-                    schemas[schema].push(section);
+					
+                    schemas[schema][section]=1;
                     done.push(changeIndex);
                 });
                 var clean = function(dataset) {
@@ -247,16 +319,14 @@
                                 delete entity[p];
                             }
                         });
-                        //FIXME: remove any properties that aren't part of the schema
-                        //except dirty and deleted?
                     });
                     return dataset;
                 };
                 var files = [];
                 Object.keys(schemas).forEach(function(schema) {
-                    schemas[schema].forEach(function(section) {
+                    Object.keys(schemas[schema]).forEach(function(section) {
                         var filename = curriculum.schemas[schema].properties[section]['#file'];
-                        files.push({ path: filename, data: JSON.stringify(clean(curriculum.data[section]), null, "\t"), schema: schema});
+                        files.push({ path: filename, data: JSON.stringify(clean(curriculum.schema[schema][section]), null, "\t"), schema: schema});
                     });
                 });
                 return new Promise(function(resolve, reject) {
@@ -271,6 +341,7 @@
                             }
                         }
                         var file = files.shift();
+						debugger;
                         return curriculum.sources[file.schema].writeFile(file.path, file.data, message).then(writeFile);
                     }
                     return writeFile();
